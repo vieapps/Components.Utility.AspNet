699612fa45b4f0680e52768c42c646d224cd50ae
Quynh Nguyen <mr.quynh@gmail.com>
2017-11-11 04:28:31 +0700

    Release new version with Redis supported

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,31 +1,22 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
-
-// General Information about an assembly is controlled through the following set of attributes.
-// Change these attribute values to modify the information associated with an assembly.
-[assembly: AssemblyTitle("VIEApps NGX")]
-[assembly: AssemblyDescription("Core components of VIEApps NGX")]
-[assembly: AssemblyDefaultAlias("VIEApps.Components.Utility")]
-[assembly: AssemblyConfiguration("Production/Stable")]
-[assembly: AssemblyCompany("VIEApps.net")]
-[assembly: AssemblyProduct("VIEApps NGX")]
-[assembly: AssemblyCopyright("Copyright © 2017 VIEApps.net")]
-[assembly: AssemblyTrademark("VIEApps")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible to COM components.
 // If you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("VIEApps NGX Utility")]
+[assembly: AssemblyCompany("VIEApps.net")]
+[assembly: AssemblyProduct("VIEApps NGX")]
+[assembly: AssemblyCopyright("© 2017 VIEApps.net")]
+
 // Version information for an assembly consists of the following four values:
-//
 //      Major Version
 //      Minor Version 
 //      Build Number
 //      Revision
-//
-[assembly: AssemblyVersion("10.1.1.1711")]
-[assembly: AssemblyFileVersion("10.1.1.1711")]
-[assembly: AssemblyInformationalVersion("10.1.nstd-2017.11.01")]
+[assembly: AssemblyVersion("10.1.3.1712")]
+[assembly: AssemblyFileVersion("10.1.3.1712")]
+[assembly: AssemblyInformationalVersion("10.1 (v10.1.nstd-2017.12.01)")]
