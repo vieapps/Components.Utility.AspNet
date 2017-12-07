50b91cc0a7e4e0b96461356d9c8eaa15574cbcc0
Quynh Nguyen <mr.quynh@gmail.com>
2017-12-07 20:28:55 +0700

    Improvement

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,22 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Setting ComVisible to false makes the types in this assembly not visible to COM components.
-// If you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// General Information about an assembly is controlled through the following set of attributes.
-// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("VIEApps NGX Utility")]
 [assembly: AssemblyCompany("VIEApps.net")]
 [assembly: AssemblyProduct("VIEApps NGX")]
 [assembly: AssemblyCopyright("© 2017 VIEApps.net")]
 
-// Version information for an assembly consists of the following four values:
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-[assembly: AssemblyVersion("10.1.3.1712")]
-[assembly: AssemblyFileVersion("10.1.3.1712")]
-[assembly: AssemblyInformationalVersion("10.1 (v10.1.nstd-2017.12.01)")]
+[assembly: AssemblyVersion("10.1.4.1801")]
+[assembly: AssemblyFileVersion("10.1.4.1801")]
+[assembly: AssemblyInformationalVersion("v10.1.netstandard-2+rev:2018.01.02")]
